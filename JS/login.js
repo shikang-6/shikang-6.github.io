ec2fe66d9b806e6d6167f6e806785461d5904a11
shikang-6 <3127123417@qq.com>
2024-07-31 11:32:30 +0800

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -2,7 +2,7 @@ function checkUser() {
   var use = document.getElementById("username");
   var pas = document.getElementById("password");
   if (use.value == 'sk' && pas.value == 250) {
-    window.location.href = "index.html"
+    window.location.href = "llog.html"
   } else {
     alert("用户或密码输入错误");
   }
